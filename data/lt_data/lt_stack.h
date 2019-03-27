a72c0e6172f0bbfd0c8a9e43a0c517d1583da66f
gaohl <20075113ghl@sina.com>
2019-03-27 22:06:28 +0800

    1. 增加了lt_stack的定义

---
+++ data/lt_data/lt_stack.h
@@ -0,0 +1,43 @@
+//
+// Created by root on 19-3-27.
+//
+
+#ifndef DATASTOR_UTILS_LT_STACK_H
+#define DATASTOR_UTILS_LT_STACK_H
+
+#include <vector>
+
+class lt_stack
+{
+
+class stack_t
+{
+    void                *private_data;
+    std::vector<void *> data_stack;
+public:
+    void push_private(void *new_private)
+    {
+        data_stack.push_back(private_data);
+        private_data = new_private;
+    }
+    
+    void *pop_private()
+    {
+        void *cur_private = private_data;
+        if ( data_stack.size() != 0 )
+        {
+            private_data = data_stack.back();
+            data_stack.pop_back();
+        }
+        return cur_private;
+    }
+    
+    void *top_private()
+    {
+        return private_data;
+    }
+};
+};
+
+
+#endif //DATASTOR_UTILS_LT_STACK_H

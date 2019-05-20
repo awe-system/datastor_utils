3fedb1c24c09b6bb11cecdd4b9aaa1e182d3e7ed
Ivan Gao <20075113ghl@sina.com>
2019-05-20 15:31:26 +0800

    1. 线程数目变成多线程

--- functions/communicate/lt_thread_server.cpp
+++ functions/communicate/lt_thread_server.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "lt_thread_server.h"
 
-lt_thread_single::lt_thread_single()
-        : _io_service(), _work(_io_service)
+lt_thread_single::lt_thread_single(boost::asio::io_service *io_service)
+        : _io_service(io_service)
 {
     th = new boost::thread(boost::bind(&lt_thread_single::run, this));
     cond.wait();
@@ -10,28 +10,28 @@ lt_thread_single::lt_thread_single()
 
 lt_thread_single::~lt_thread_single()
 {
-    _io_service.stop();
+    _io_service->stop();
     th->join();
     delete th;
 }
 
 boost::asio::io_service *lt_thread_single::get_io_service()
 {
-    return &_io_service;
+    return _io_service;
 }
 
 void lt_thread_single::run()
 {
     cond.notify();
-    _io_service.run();
+    _io_service->run();
 }
 
-lt_thread_server::lt_thread_server(int thread_num) : curret_cnt(0),
-                                                     splck()
+lt_thread_server::lt_thread_server(int thread_num) :
+        curret_cnt(0), splck(), _io_service(),_work(_io_service)
 {
     for ( int i = 0; i < thread_num; ++i )
     {
-        lt_thread_single *t = new lt_thread_single();
+        lt_thread_single *t = new lt_thread_single(&_io_service);
         threads.push_back(t);
     }
 }
@@ -39,7 +39,8 @@ lt_thread_server::lt_thread_server(int thread_num) : curret_cnt(0),
 boost::asio::io_service *lt_thread_server::get_io_service()
 {
     boost::lock_guard<boost::detail::spinlock> lck(splck);
-    if(threads.empty()) return nullptr;
+    if ( threads.empty() )
+    { return nullptr; }
     if ( ++curret_cnt == threads.size() )
     { curret_cnt = 0; }
     return threads[curret_cnt]->get_io_service();
@@ -48,7 +49,7 @@ boost::asio::io_service *lt_thread_server::get_io_service()
 lt_thread_server::~lt_thread_server()
 {
     boost::lock_guard<boost::detail::spinlock> lck(splck);
-    while(!threads.empty())
+    while ( !threads.empty() )
     {
         delete threads.back();
         threads.pop_back();
--- functions/communicate/lt_thread_server.h
+++ functions/communicate/lt_thread_server.h
@@ -8,14 +8,13 @@
 
 class lt_thread_single
 {
-    boost::asio::io_service _io_service;
-    boost::asio::io_service::work _work;
+    boost::asio::io_service *_io_service;
     boost::thread* th;
     lt_condition cond;
 public:
     void run();
 public:
-    lt_thread_single();
+    lt_thread_single(boost::asio::io_service *io_service);
     boost::asio::io_service *get_io_service();
     ~lt_thread_single();
 };
@@ -23,6 +22,8 @@ public:
 class lt_thread_server
 {
 private:
+    boost::asio::io_service _io_service;
+    boost::asio::io_service::work _work;
     boost::detail::spinlock splck;
     int curret_cnt;
     std::vector<lt_thread_single *> threads;

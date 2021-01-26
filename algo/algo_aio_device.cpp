0a698ef3c7f6238038763ae5c1f360074796be87
li.jun <li.jun@cloudsdnet.com>
2021-01-26 14:56:42 +0800

    change  max epoll event value

--- algo/algo_aio_device.cpp
+++ algo/algo_aio_device.cpp
@@ -10,7 +10,7 @@ namespace ServerSan_Algo {
 algo_aio_device::algo_aio_device(const string ser, const string path, unsigned long size_secs,
                                  libaio_device_service *aio_server) :
         aio_server_(aio_server),serial_num_(ser), path_(path), sector_num_(size_secs),
-    aio_dev_(path, 256, aio_server, std::bind(&algo_aio_device::req_done, this, std::placeholders::_1,
+    aio_dev_(path, 4096, aio_server, std::bind(&algo_aio_device::req_done, this, std::placeholders::_1,
                                                      std::placeholders::_2)){}
 
 
